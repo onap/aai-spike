0ed3574abdc6e7198075e1de0699e6fb358c2baf
rajeevme <rajeev.mehta@amdocs.com>
2019-10-17 12:55:19 +0530

    [AAI-2181] Change aai spike container processes to run as non-root on the host
    Issue-ID: AAI-2181
    
    Change-Id: Ifd6571190f1eaf473728010d870d2ea067dbed73
    
    Signed-off-by: rajeevme<rajeev.mehta@amdocs.com>
    Change-Id: I8026cc1962f794e110fc6c5c2e9c5075a0e28798

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -23,5 +23,33 @@ PROPS="$PROPS -Dlogging.config=$APP_HOME/bundleconfig/etc/logback.xml"
 PROPS="$PROPS -DKEY_STORE_PASSWORD=$KEY_STORE_PASSWORD"
 JVM_MAX_HEAP=${MAX_HEAP:-1024}
 
+# Changes related to:AAI-2181
+# Change aai spike container processes to run as non-root on the host
+USER_ID=${LOCAL_USER_ID:-9001}
+GROUP_ID=${LOCAL_GROUP_ID:-9001}
+SPK_LOGS=/var/log/onap/AAI-SPK
+
+if [ $(cat /etc/passwd | grep aaiadmin | wc -l) -eq 0 ]; then
+
+        groupadd aaiadmin -g ${GROUP_ID} || {
+                echo "Unable to create the group id for ${GROUP_ID}";
+                exit 1;
+        }
+        useradd --shell=/bin/bash -u ${USER_ID} -g ${GROUP_ID} -o -c "" -m aaiadmin || {
+                echo "Unable to create the user id for ${USER_ID}";
+                exit 1;
+        }
+fi;
+
+chown -R aaiadmin:aaiadmin ${MICRO_HOME}
+chown -R aaiadmin:aaiadmin ${APP_HOME}
+chown -R aaiadmin:aaiadmin ${SPK_LOGS}
+
+find ${MICRO_HOME}  -name "*.sh" -exec chmod +x {} +
+
+gosu aaiadmin ln -s /logs $MICRO_HOME/logs
+JAVA_CMD="exec gosu aaiadmin java";
+
+
 set -x
-exec java -Xmx${JVM_MAX_HEAP}m $PROPS -jar ${APP_HOME}/spike.jar
+${JAVA_CMD} -Xmx${JVM_MAX_HEAP}m $PROPS -jar ${APP_HOME}/spike.jar
